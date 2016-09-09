26ed63b25c97742039301b7896ae137e63f5d901
tidjungs <waewprach.s@ku.th>
2016-09-09 14:22:44 +0700

    initial project template

---
+++ GameBoard.java
@@ -0,0 +1,102 @@
+package sokoban;
+
+public class GameBoard {
+	  	private int height;
+	  	private int width;
+	  	private String[] baseBoard;
+	 
+	    private int playerRow;
+	    private int playerCol;
+	 
+	    private int numBoxes;
+	    private int[] boxRows;
+	    private int[] boxCols;
+	 
+	    public GameBoard(String[] map) {
+	        loadBoard(map);
+	    }
+	 
+	    public void loadBoard(String[] map) {
+	        height = map.length;
+	        width = map[0].length();
+	        numBoxes = 0;
+	        boxRows = new int[height*width];
+	        boxCols = new int[height*width];
+	 
+	        baseBoard = new String[height];
+	        for(int r = 0; r < height; r++) {
+	            baseBoard[r] = "";
+	            for(int c = 0; c < width; c++) {
+	                char mch = map[r].charAt(c);
+	                char sch = '.';
+	                switch(mch) {
+	                case 'A': 
+	                    playerRow = r;
+	                    playerCol = c;
+	                    break;
+	                case 'O':
+	                    boxRows[numBoxes] = r;
+	                    boxCols[numBoxes] = c;
+	                    numBoxes++;
+	                    break;
+	                default:
+	                    sch = mch;
+	                }
+	                baseBoard[r] += sch;
+	            }
+	        }
+	    }
+	 
+	    public int getHeight() {
+	        return height;
+	    }
+	 
+	    public int getWidth() {
+	        return width;
+	    }
+	 
+	    public int getPlayerRow() {
+	        return playerRow;
+	    }
+	 
+	    public int getPlayerCol() {
+	        return playerCol;
+	    }
+	 
+	    public void setPlayerPosition(int r, int c) {
+	        playerRow = r;
+	        playerCol = c;
+	    }
+	 
+	    public int getNumBoxes() {
+	        return numBoxes;
+	    }
+	 
+	    public int[] getBoxPosition(int i) {
+	        return new int[] { 
+	                boxRows[i],
+	                boxCols[i]
+	        };
+	    }
+	 
+	    public void setBoxPosition(int i, int r, int c) {
+	        boxRows[i] = r;
+	        boxCols[i] = c;
+	    }
+	 
+	    public boolean hasPlayerAt(int r, int c) {
+	        return (playerRow == r) && (playerCol == c);
+	    }
+	 
+	    public boolean hasBoxAt(int r, int c) {
+	        return false;
+	    }
+	 
+	    public boolean hasExitAt(int r, int c) {
+	        return false;
+	    }
+	 
+	    public String toString() {
+	        return "";
+	    }
+}
---
+++ GameBoardTest.java
@@ -0,0 +1,60 @@
+package sokoban;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class GameBoardTest {
+
+	 static String smallBoardMap[] = {
+		        " #####",
+		        "#*O.A#",
+		        "#...O#",
+		        "##..*#",
+		        " #####"
+		    };
+		    private GameBoard smallBoard;
+		 
+		    @Before
+		    public void setUp() {
+		        smallBoard = new GameBoard(smallBoardMap);
+		    }
+		 
+		    @Test
+		    public void testLoadBoardProperties() {
+		        assertEquals(5, smallBoard.getHeight());
+		        assertEquals(6, smallBoard.getWidth());
+		    }
+		 
+		    @Test
+		    public void testLoadBoardPlayerPosition() {
+		        assertEquals(1, smallBoard.getPlayerRow());
+		        assertEquals(4, smallBoard.getPlayerCol());
+		    }
+		 
+		    @Test
+		    public void testLoadBoardNumBoxes() {
+		        assertEquals(2, smallBoard.getNumBoxes());
+		    }
+		 
+		    @Test
+		    public void testLoadBoardBoxPositions() {
+		        assertArrayEquals(new int[] {1, 2}, smallBoard.getBoxPosition(0));
+		        assertArrayEquals(new int[] {2, 4}, smallBoard.getBoxPosition(1));
+		    }    
+		 
+		    @Test
+		    public void testPlayerPositionCheck() {
+		        assertFalse(smallBoard.hasPlayerAt(0,0));
+		        assertTrue(smallBoard.hasPlayerAt(1,4));
+		    }
+		 
+		    @Test
+		    public void testPlayerPositionCheckAfterChange() {
+		        smallBoard.setPlayerPosition(2, 3);
+		        assertFalse(smallBoard.hasPlayerAt(1,4));
+		        assertTrue(smallBoard.hasPlayerAt(2, 3));
+		    }
+
+}
